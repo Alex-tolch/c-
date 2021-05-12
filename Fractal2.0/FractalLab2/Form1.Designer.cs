a1770ed809eb108fb7112f48c8d03f38445bb10d
Alex-tolch <84074656+Alex-tolch@users.noreply.github.com>
2021-05-12 14:30:50 +0400

    Add files via upload

---
+++ Fractal2.0/FractalLab2/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace FractalLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.field = new System.Windows.Forms.PictureBox();
+            this.AxiomText = new System.Windows.Forms.TextBox();
+            this.drawbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.aboutbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.field)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // field
+            // 
+            this.field.BackColor = System.Drawing.Color.White;
+            this.field.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.field.Location = new System.Drawing.Point(0, 0);
+            this.field.Margin = new System.Windows.Forms.Padding(2);
+            this.field.Name = "field";
+            this.field.Size = new System.Drawing.Size(1028, 502);
+            this.field.TabIndex = 0;
+            this.field.TabStop = false;
+            // 
+            // AxiomText
+            // 
+            this.AxiomText.Location = new System.Drawing.Point(160, 13);
+            this.AxiomText.Margin = new System.Windows.Forms.Padding(2);
+            this.AxiomText.Multiline = true;
+            this.AxiomText.Name = "AxiomText";
+            this.AxiomText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.AxiomText.Size = new System.Drawing.Size(251, 341);
+            this.AxiomText.TabIndex = 1;
+            this.AxiomText.TextChanged += new System.EventHandler(this.AxiomText_TextChanged);
+            // 
+            // drawbutton
+            // 
+            this.drawbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.drawbutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.drawbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.drawbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.drawbutton.Location = new System.Drawing.Point(11, 259);
+            this.drawbutton.Margin = new System.Windows.Forms.Padding(2);
+            this.drawbutton.Name = "drawbutton";
+            this.drawbutton.Size = new System.Drawing.Size(114, 31);
+            this.drawbutton.TabIndex = 2;
+            this.drawbutton.Text = "Проход";
+            this.drawbutton.UseVisualStyleBackColor = false;
+            this.drawbutton.Click += new System.EventHandler(this.drawbutton_Click);
+            this.drawbutton.Move += new System.EventHandler(this.drawbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(345, 375);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(200, 375);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Кол-во проходов:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // aboutbutton
+            // 
+            this.aboutbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.aboutbutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.aboutbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.aboutbutton.Location = new System.Drawing.Point(11, 294);
+            this.aboutbutton.Margin = new System.Windows.Forms.Padding(2);
+            this.aboutbutton.Name = "aboutbutton";
+            this.aboutbutton.Size = new System.Drawing.Size(114, 28);
+            this.aboutbutton.TabIndex = 5;
+            this.aboutbutton.Text = "Задание";
+            this.aboutbutton.UseVisualStyleBackColor = false;
+            this.aboutbutton.Click += new System.EventHandler(this.aboutbutton_Click);
+            this.aboutbutton.Move += new System.EventHandler(this.aboutbutton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(40, 13);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Правила: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 27);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Очистить поля";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Move += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.progressBar1.ForeColor = System.Drawing.Color.Aqua;
+            this.progressBar1.Location = new System.Drawing.Point(160, 420);
+            this.progressBar1.MarqueeAnimationSpeed = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.progressBar1.Size = new System.Drawing.Size(236, 23);
+            this.progressBar1.Step = 5;
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.UseWaitCursor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Fractal 2.0";
+            this.notifyIcon1.Visible = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox1.Location = new System.Drawing.Point(44, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox2.Location = new System.Drawing.Point(44, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox4.Location = new System.Drawing.Point(44, 142);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "X";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Угол";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(9, 235);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(125, 129);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Действия";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1028, 502);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.aboutbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawbutton);
+            this.Controls.Add(this.AxiomText);
+            this.Controls.Add(this.field);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fractal 2.0";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.field)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox field;
+        private System.Windows.Forms.TextBox AxiomText;
+        private System.Windows.Forms.Button drawbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button aboutbutton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Fractal2.0/FractalLab2/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FractalLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "YF+XF+Y";
+            textBox2.Text = "XF-YF-X";
+            AxiomText.Text = "YF";
+            textBox4.Text = "60";
+            AutoCompleteStringCollection source = new AutoCompleteStringCollection()
+        {
+            "10","20","30","40","50","60","70","80","90"
+        };
+            textBox4.AutoCompleteCustomSource = source;
+            textBox4.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox4.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection source1 = new AutoCompleteStringCollection()
+        {
+            "X[-FFF][+FFF]FX","YF+XF+Y"
+        };
+            textBox1.AutoCompleteCustomSource = source1;
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection source2 = new AutoCompleteStringCollection()
+        {
+            "YFX[+Y][-Y]","XF-YF-X"
+        };
+            textBox2.AutoCompleteCustomSource = source2;
+            textBox2.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+
+            AutoCompleteStringCollection source3 = new AutoCompleteStringCollection()
+        {
+            "XF-F+F-XF+F+XF-F+F-X"
+        };
+            textBox1.AutoCompleteCustomSource = source3;
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+        int steps = 0;
+
+
+        /// <summary>
+        /// Метод, выполняющий рисовку фрактала. Использует последовательность действий из TextBоx, поверхность, на которую наносится рисунок, коэффицент, задающий значения уменьшения рисуемой линии с увеличением кол-ва шагов и кисть.
+        /// </summary>
+        /// <param name="axioma"></param>
+        /// <param name="q"></param>
+        /// <param name="H"></param>
+        /// <param name="brush"></param>
+        public void DrawFractal(String axioma, Graphics q, double H,Pen brush)
+        {
+            Stack<string> stack = new Stack<string>();
+            double L = 0;
+            char[] DrawMap = axioma.ToCharArray();// перевод символов, обозначающих последовательность действий, в массив символов
+            float x0 = field.Width/3+40;// начальная координата x
+            float y0 = field.Height/3+150;// начальная координата y
+            float x1 = x0;
+            float y1 = y0;
+            
+            for (int i = 0; i < DrawMap.Length; i++)// проход всех символов из TextBox
+            {
+                brush.Color = Color.DarkViolet;
+                switch (DrawMap[i])// выборка для каждого эл-та
+                {
+                    case 'F':
+                        {
+                            x1 += (float)(2 * H * Math.Cos((L * Math.PI) / 180));
+                            y1 += (float)(2 * H * Math.Sin((L * Math.PI) / 180));
+                            q.DrawLine(brush, x0, y0, x1, y1);
+                            x0 = x1;
+                            y0 = y1;
+                            break;
+                        }
+                    case 'X':
+                        {
+                            break;
+                        }
+                    case 'Y':
+                        {
+                            break;
+                        }
+                    case '-':
+                        {
+                            L -= Convert.ToDouble(textBox4.Text);
+                            break;
+                        }
+                    case '+':
+                        {
+                            L += Convert.ToDouble(textBox4.Text);
+                            break;
+                        }
+                    
+                    case '[':
+                        {
+                            stack.Push(Convert.ToString(x0));
+                            stack.Push(Convert.ToString(y0));
+                            stack.Push(Convert.ToString(L));
+                            break;
+                        }
+                    case ']':
+                        {
+                            L = float.Parse(stack.Pop());
+                            y0 = float.Parse(stack.Pop());
+                            x0 = float.Parse(stack.Pop());
+                            x1 = x0;
+                            y1 = y0;
+                            
+                            break;
+                        }
+                }
+            }   
+        }
+
+        public string ToTextFormula(string before, string X,string Y)
+        {
+            string Nextsteps = before;
+            int k = 0;
+            char[] DrawMap = Nextsteps.ToCharArray();
+            for (int i = 0; i < DrawMap.Length; i++)
+            {
+                if (DrawMap[i] == 'X')
+                {
+                    Nextsteps = Nextsteps.Remove(k, 1);
+                    Nextsteps = Nextsteps.Insert(k, X);
+                    k += X.Length;
+                    continue;
+                }
+                if (DrawMap[i] == 'Y')
+                {
+                    Nextsteps = Nextsteps.Remove(k, 1);
+                    Nextsteps = Nextsteps.Insert(k, Y);
+                    k += Y.Length;
+                    continue;
+                }
+                k++;
+            }
+            return Nextsteps;
+        }
+
+        
+        /// <summary>
+        /// Основная часть программы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void drawbutton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                progressBar1.Value = i;
+                Thread.Sleep(10);
+            }
+            
+            steps++;
+                label1.Text = Convert.ToString(steps);
+                AxiomText.Text = ToTextFormula(AxiomText.Text, textBox1.Text, textBox2.Text);
+                Graphics g = field.CreateGraphics();
+                g.Clear(Color.White);
+                Pen pen = new Pen(Color.DarkViolet, 2);
+                DrawFractal(AxiomText.Text, g,  (35/Math.Pow(steps, 2)*1.5), pen);
+            progressBar1.Value = 0;
+        } 
+
+        private void aboutbutton_Click(object sender, EventArgs e)
+        {
+            About about = new About();
+            about.ShowDialog();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AxiomText.Text = "YF";
+            Graphics g = field.CreateGraphics();
+            g.Clear(Color.White);
+            steps = 0;
+            label1.Text = Convert.ToString('0');
+        }
+
+        private void AxiomText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
