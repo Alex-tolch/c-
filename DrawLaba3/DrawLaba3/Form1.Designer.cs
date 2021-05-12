476431a376595ed29e10cbec9f67e9b8bb3f190d
Alex-tolch <84074656+Alex-tolch@users.noreply.github.com>
2021-05-12 14:30:05 +0400

    Add files via upload

---
+++ DrawLaba3/DrawLaba3/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace DrawLaba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(604, 395);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Линия";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(84, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ломаная";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(276, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(255, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 44);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(11, 36);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Круг";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(84, 39);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(77, 17);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Заливка 1";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "label3";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(11, 60);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(55, 17);
+            this.radioButton5.TabIndex = 14;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Кисть";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(86, 63);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(106, 17);
+            this.radioButton6.TabIndex = 15;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Заливка Модиф";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(456, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(11, 84);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(114, 17);
+            this.radioButton7.TabIndex = 17;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Закраска узором";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(11, 108);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(97, 17);
+            this.radioButton8.TabIndex = 18;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Безье прямая";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 395);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+    }
+}
+
---
+++ DrawLaba3/DrawLaba3/Form1.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLaba3
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            // расширенное окно для выбора цвета
+            // colorDialog1.FullOpen = true;
+            // установка обработчика события Scroll
+           
+        }
+
+        public void SimpleLine(double x, double y, double X, double Y, Bitmap bmp, Color color)
+        {
+            double x0;
+            double x1;
+            double y0;
+            double y1;
+
+            bool flag = false;
+
+            if ((X - x) == 0)
+            {
+                y0 = (y < Y) ? y : Y;
+                y1 = (y < Y) ? Y : y;
+                for (int i = Convert.ToInt32(y0); i <= y1; i++)
+                {
+                    bmp.SetPixel(Convert.ToInt32(x), i, color);
+
+                }
+            }
+            else
+            {
+                double a = (Y - y) / (X - x);
+                double b = y - a * x;
+                if (Math.Abs(X - x) < Math.Abs(Y - y))
+                {
+                    // Меняем координату у
+                    x0 = y;
+                    x1 = Y;
+                    y0 = x;
+                    y1 = X;
+                    flag = true;
+                }
+                else
+                {
+                    // Меняем координату х
+                    x0 = x;
+                    x1 = X;
+                    y0 = y;
+                    y1 = Y;
+                }
+
+                if (x1 < x0)
+                {
+                    double v = x1; x1 = x0; x0 = v;
+                }
+
+                if (flag)
+                {
+                    for (int i = Convert.ToInt32(x0); i <= x1; i++) bmp.SetPixel(Convert.ToInt32((i - b) / a), i, color);
+                }
+                else
+                    for (int i = Convert.ToInt32(x0); i <= x1; i++) bmp.SetPixel(i, Convert.ToInt32(i * a + b), color);
+
+            }
+        }
+
+        public void BarezenhamRound(int x0, int y0, int R, Bitmap bmp, Color color)
+        {
+            int x = 0;
+            int y = R;
+            int d = 3 - 2 * R;
+            while (y >= x)
+            {
+                bmp.SetPixel(x + x0, y + y0, color);
+                bmp.SetPixel(x + x0, -y + y0, color);
+                bmp.SetPixel(-x + x0, y + y0, color);
+                bmp.SetPixel(-x + x0, -y + y0, color);
+                bmp.SetPixel(y + x0, x + y0, color);
+                bmp.SetPixel(y + x0, -x + y0, color);
+                bmp.SetPixel(-y + x0, x + y0, color);
+                bmp.SetPixel(-y + x0, -x + y0, color);
+                if (d <= 0)
+                {
+                    d += 4 * x + 6;
+                }
+                else
+                {
+                    d += 4 * (x - y) + 10;
+                    y--;
+                }
+                x++;
+            }
+        }
+        public void ModifyPattern(int x0, int y0, Bitmap pic, Color color)
+        {
+            Color bgColor = pic.GetPixel(x0, y0);
+            pic.SetPixel(x0, y0, color);
+            if (pic.GetPixel(x0 + 1, y0) == bgColor)
+            {
+                ModifyPattern(x0 + 1, y0, pic, color);
+            }
+            if (pic.GetPixel(x0 - 1, y0) == bgColor)
+            {
+                ModifyPattern(x0 - 1, y0, pic, color);
+            }
+            if (pic.GetPixel(x0, y0 + 1) == bgColor)
+            {
+                ModifyPattern(x0, y0 + 1, pic, color);
+            }
+            if (pic.GetPixel(x0, y0 - 1) == bgColor)
+            {
+                ModifyPattern(x0, y0 - 1, pic, color);
+            }
+        }
+        public void Pattern(int x, int y, Bitmap bmp, Color color)
+        {
+            Color backcolor = bmp.GetPixel(x, y);
+            int xl = x;
+            int xr = x + 1;
+            while ((xl >= 0) && (bmp.GetPixel(xl, y) == backcolor))
+            {
+                bmp.SetPixel(xl, y, color);
+                xl--;
+            }
+            xl++;
+            while ((xr < bmp.Width - 1) && (bmp.GetPixel(xr, y) == backcolor))
+            {
+                bmp.SetPixel(xr, y, color);
+                xr++;
+            }
+            xr--;
+
+            int tmp_x = xl;
+            while ((tmp_x <= xr) && (y != 0))
+            {
+               
+                while ((tmp_x <= xr) && (bmp.GetPixel(tmp_x, y - 1) != backcolor))
+                {
+                    tmp_x++;
+                }
+                if (tmp_x <= xr) Pattern(tmp_x, y - 1, bmp, color);
+                tmp_x++;
+            }
+            tmp_x = xl;
+            while ((tmp_x <= xr) && (y + 1 != bmp.Height))
+            {
+                while ((tmp_x <= xr) && (bmp.GetPixel(tmp_x, y + 1) != backcolor))
+                {
+                    tmp_x++;
+                }
+                if (tmp_x <= xr) Pattern(tmp_x, y + 1, bmp, color);
+                tmp_x++;
+            }
+        }
+
+        public void PatternLines(int x, int y, Bitmap bmp, Color[,] colorZ, int w, int h)
+        {
+            Color backcolor = bmp.GetPixel(x, y);
+            int xl = x;
+            int xr = x + 1;
+            while ((xl >= 0) && (bmp.GetPixel(xl, y) == backcolor))
+            {
+                bmp.SetPixel(xl, y, colorZ[xl % w, y % h]);
+                xl--;
+            }
+            xl++;
+            while ((xr < bmp.Width - 1) && (bmp.GetPixel(xr, y) == backcolor))
+            {
+                bmp.SetPixel(xr, y, colorZ[xr % w, y % h]);
+                xr++;
+            }
+            xr--;
+
+            int tmp_x = xl;
+            while ((tmp_x <= xr) && (y != 0))
+            {
+                while ((tmp_x <= xr) && (bmp.GetPixel(tmp_x, y - 1) != backcolor))
+                {
+                    tmp_x++;
+                }
+                if (tmp_x <= xr) PatternLines(tmp_x, y - 1, bmp, colorZ, w, h);
+                tmp_x++;
+            }
+            tmp_x = xl;
+            while ((tmp_x <= xr) && (y + 1 != bmp.Height))
+            {
+                while ((tmp_x <= xr) && (bmp.GetPixel(tmp_x, y + 1) != backcolor))
+                {
+                    tmp_x++;
+                }
+                if (tmp_x <= xr) PatternLines(tmp_x, y + 1, bmp, colorZ, w, h);
+                tmp_x++;
+            }
+        }
+        public void BezierLine(PointF[] points, Bitmap bmp, Color color)
+        {
+            int n = points.Length;
+            int countPoints = n<< 6;
+            PointF[] P = new PointF[n];
+            PointF[] drawingPoints = new PointF[countPoints + 1];
+            float dt = 1f / countPoints;
+            float t = 0f;
+            for (int i = 0; i <= countPoints; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    P[j] = points[j];
+                }
+
+                for (int j = n - 1; j > 0; j--)
+                {
+                    for (int k = 0; k < j; k++)
+                    {
+                        P[k].X += t * (P[k + 1].X - P[k].X);
+                        P[k].Y += t * (P[k + 1].Y - P[k].Y);
+                    }
+                }
+                drawingPoints[i] = P[0];
+                t += dt;
+            }
+            for (int i = 1; i < countPoints + 1; i++)
+            {
+                SimpleLine((int)drawingPoints[i - 1].X, (int)drawingPoints[i - 1].Y, (int)drawingPoints[i].X, (int)drawingPoints[i].Y, bmp, color);
+            }
+        }
+
+
+
+        bool flag = false;
+        int x, y, X, Y;
+        Color color = Color.FromArgb(1, 1, 1);
+        bool temp1 = false;
+        bool p1, p2 = false;
+        
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            temp1 = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            for (int i = 0; i < pictureBox1.Width; i++)
+                for (int j = 0; j < pictureBox1.Height; j++)
+                {
+                    map.SetPixel(i, j, Color.WhiteSmoke);
+                    pictureBox1.Image = map;
+                }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.AllowFullOpen = true;
+            colorDialog1.ShowHelp = true;
+
+
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color = colorDialog1.Color;
+                groupBox1.BackColor = color;
+            }
+
+            //if (colorDialog1.ShowDialog() == DialogResult.OK)
+            //    // установка цвета
+            //    color = colorDialog1.Color;
+            //    groupBox1.BackColor = color;
+        }
+        Bitmap bmp = new Bitmap(1376,762);
+        PointF[] points = new PointF[3];
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        { try
+            {
+                Color[,] col = new Color[2, 2];
+                col[0, 0] = Color.White;
+                col[0, 1] = color;
+                col[1, 0] = color;
+                col[1, 1] = Color.White;
+                int h = 2;
+                int w = 1;
+               
+                x = e.X;
+                y = e.Y;
+                int x1, x2, y1, y2;
+                ///Риование линии
+                if (radioButton1.Checked)
+                {
+                    if (flag)
+                    {
+                        SimpleLine(x, y, X, Y, bmp, color);
+                        pictureBox1.Image = bmp;
+                        X = x;
+                        Y = y;
+                        flag = false;
+                    }
+                    else
+                    {
+                        X = x;
+                        Y = y;
+                        bmp.SetPixel(x, y, color);
+                        pictureBox1.Image = bmp;
+                        flag = true;
+                    }
+
+                }
+                ///Рисование ломаной линии
+                if (radioButton2.Checked)
+                {
+                    if (flag)
+                    {
+                        SimpleLine(x, y, X, Y, bmp, color);
+                        pictureBox1.Image = bmp;
+                        X = x;
+                        Y = y;
+                    }
+                    else
+                    {
+                        X = x;
+                        Y = y;
+                        bmp.SetPixel(x, y, color);
+                        pictureBox1.Image = bmp;
+                        flag = true;
+                    }
+
+                }
+                //круг
+                if (radioButton3.Checked)
+                {
+                    if (flag)
+                    {
+                        BarezenhamRound(X, Y, Convert.ToInt32(Math.Abs(Math.Sqrt(Math.Pow((X - x), 2) + Math.Pow((Y - y), 2)))), bmp, color);
+                        pictureBox1.Image = bmp;
+                        bmp.SetPixel(X, Y, color);
+                        pictureBox1.Image = bmp;
+                        flag = false;
+                    }
+                    else
+                    {
+                        X = x;
+                        Y = y;
+                        bmp.SetPixel(x, y, color);
+                        pictureBox1.Image = bmp;
+                        flag = true;
+                    }
+                }
+                //заливка
+                if (radioButton4.Checked)
+                    Pattern(x, y, bmp, color);
+                pictureBox1.Image = bmp;
+                //ручка
+                if (radioButton5.Checked)
+                    temp1 = true;
+                //модиф заливка
+                if (radioButton6.Checked)
+                {
+                    try
+                    {
+                        ModifyPattern(Convert.ToInt32(x), Convert.ToInt32(y), bmp, color);
+                        pictureBox1.Image = bmp;
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Стек, при закраске этим методом переполнен!");
+                    }
+                }
+                //заливка узором
+                if(radioButton7.Checked)
+                {
+                    PatternLines(x, y, bmp, col, w, h);
+                    pictureBox1.Image = bmp;
+                }
+                //линия безье
+                if (radioButton8.Checked)
+                {
+                    if (p2)
+                    {
+                        points[0] = new PointF(x, y);
+                        BezierLine(points, bmp, color);
+                        pictureBox1.Image = bmp;
+                        p1 = false;
+                        p2 = false;
+                    }
+                    else
+                    {
+                        if(p1)
+                        {
+                            x2 = x;
+                            y2 = y;
+                            points[1] = new PointF(x2, y2);
+                            bmp.SetPixel(x2, y2,color);
+                            pictureBox1.Image = bmp;
+                            p2 = true;
+                        }
+                        else
+                        {
+                            x1 = x;
+                            y1 = y;
+                            points[2] = new PointF(x1, y1);
+                            bmp.SetPixel(x1, y1, color);
+                            pictureBox1.Image = bmp;
+                            p1 = true;
+                        }
+                    }
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+
+            }
+       
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+          
+            label2.Text = String.Format("X: " + e.X);
+            label3.Text = String.Format("Y: " + e.Y);
+            if (temp1 == true)
+            {
+                if (radioButton5.Checked)
+                {
+                    bmp.SetPixel(e.X, e.Y,color);
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+    }
+}
+
